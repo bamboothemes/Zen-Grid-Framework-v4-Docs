5056ad23cb98e36b8348de6148aec0ae16b8708a
Anthony Olsen <design@joomlabamboo.com>
2016-01-03 15:35:24 +1100

    Gitignore and delete cache

--- cache/compiled/files/32e92ed8cbfed5f6e09b090f18e98f0a.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'user/config/plugins/highlight.yaml',
-    'modified' => 1439346820,
-    'data' => [
-        'theme' => 'learn'
-    ]
-];
--- cache/compiled/files/f7f2f14637c40683093bee7019721f0f.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'user/config/plugins/anchors.yaml',
-    'modified' => 1439346820,
-    'data' => [
-        'selectors' => '#body h2, #body h3, #body h4, #body h5'
-    ]
-];
